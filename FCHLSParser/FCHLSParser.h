d62d9b8a0885f830f20082842a1d737e994a8ee8
Fabian Canas <fcanas@gmail.com>
2017-02-25 12:13:24 -0500

    Initial commit

---
+++ FCHLSParser/FCHLSParser.h
@@ -0,0 +1,19 @@
+//
+//  FCHLSParser.h
+//  FCHLSParser
+//
+//  Created by Fabian Canas on 2/25/17.
+//  Copyright © 2017 Fabián Cañas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FCHLSParser.
+FOUNDATION_EXPORT double FCHLSParserVersionNumber;
+
+//! Project version string for FCHLSParser.
+FOUNDATION_EXPORT const unsigned char FCHLSParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FCHLSParser/PublicHeader.h>
+
+
---
+++ FCHLSParserTests/FCHLSParserTests.m
@@ -0,0 +1,39 @@
+//
+//  FCHLSParserTests.m
+//  FCHLSParserTests
+//
+//  Created by Fabian Canas on 2/25/17.
+//  Copyright © 2017 Fabián Cañas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FCHLSParserTests : XCTestCase
+
+@end
+
+@implementation FCHLSParserTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
